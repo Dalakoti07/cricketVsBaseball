98dab38940ae68d9620e2e6fcab5340b80a83aa7
dalakoti07 <saurabhdalakoti07@gmail.com>
2020-05-09 19:50:15 +0530

    app deployed and readme made

---
+++ README.md
@@ -0,0 +1,20 @@
+# Cricket OR baseball
+
+    This a small yet interesting (at least to me :) ) for using AI to distinguish between Cricket vs baseball
+
+    Training is done in Keras, the workflow to produce the result is quite simple and uses the simple fine tuning approach
+
+### Project is deplayed in Heroku @ https://cob-07.herokuapp.com/
+
+    The deloyment is done in flask as a web service, and UI using bootstrap
+    
+    Code and approach for generating production level result can be viewed in application.py
+
+## How to run it in your own environment
+    Make a pip virtua; Environment https://packaging.python.org/guides/installing-using-pip-and-virtual-environments/
+
+    Activate Virtual Environment
+
+    Then in project root run command " python application.py "
+
+    You are good to go, please raise an issue
--- application.py
+++ application.py
@@ -1,68 +1,95 @@
-from flask import Flask, session,redirect,url_for,render_template,request,jsonify
+from flask import Flask,redirect,url_for,render_template,request,jsonify
 import requests
-from tensorflow.keras.models import model_from_json
+app = Flask(__name__)
+from PIL import Image
+import json
+import numpy as np
+import os
+# os.system('apt update && apt install -y libsm6 libxext6')
+from cv2 import imread, resize  
+import tensorflow as tf
+from flask import send_file
+from keras.models import model_from_json
+from keras.preprocessing.image import img_to_array
+from flask import send_from_directory
 
 json_file = open('model.json','r')
 loaded_model_json = json_file.read()
 json_file.close()
+
+# some fixes 
+graph=None
+graph = tf.get_default_graph()
+
 loaded_model = model_from_json(loaded_model_json)
 #load woeights into new model
-loaded_model.load_weights("ReTraining.h5")
+loaded_model.load_weights("Retraining.h5")
 print("Loaded Model from disk")
 
-app = Flask(__name__)
+@app.route('/favicon.ico') 
+def favicon(): 
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.png', mimetype='image/vnd.microsoft.icon')
 
-# from keras.optimizers import Adam
-from keras.preprocessing.image import img_to_array
-import numpy as np
-import argparse
-import random
-import cv2
-import os
-import requests
+@app.route('/about',methods=['GET'])
+def results():
+	return jsonify("U are in home page, there is nothing here, go for /post and upload image")
 
-def weAreLive():
-    pic_url=input('enter the url ')
-    with open('test.jpg', 'wb') as handle:
-            response = requests.get(pic_url, stream=True)
+def weAreLive(url):
+	pic_url=url
+	with open('test.jpg', 'wb') as handle:
+	        response = requests.get(pic_url, stream=True)
 
-            if not response.ok:
-                print (response)
+	        if not response.ok:
+	            print (response)
 
-            for block in response.iter_content(1024):
-                if not block:
-                    break
+	        for block in response.iter_content(1024):
+	            if not block:
+	                break
 
-                handle.write(block)
-#     preprocessing the image 
-    image = cv2.imread('test.jpg')
-    image = cv2.resize(image, (160,160))
-    image = img_to_array(image)
-#     cv2.imshow('ImageWindow',image)
-#     cv2.waitKey()
-#         Making the predictions
-    res=loaded_model.predict(np.expand_dims(image, axis=0))
-    if res[0][0]>res[0][1]:
-        return "Baseball"
-    else:
-        return "Cricket"
-#     deleting the file at last
-    os.remove("test.jpg")
-    
-
+	            handle.write(block)
+	#     preprocessing the image 
+	image = imread('test.jpg')
+	image = resize(image, (160,160))
+	image = img_to_array(image)
+	image= np.array(image, dtype="float") / 255.0
+	#     cv2.imshow('ImageWindow',image)
+	#     cv2.waitKey()
+	#         Making the predictions
+	img=np.expand_dims(image, axis=0)
+	# clear_session()
+	global loaded_model
+	# loaded_model._make_predict_function()
+	with graph.as_default():
+		res=loaded_model.predict(img)
+	os.remove("test.jpg")
+	print("prediction from model is ",res)
+	predictionResults={}
+	predictionResults["Baseball"]=str(res[0][0])
+	predictionResults["Cricket"]=str(res[0][1])
+	
+	return predictionResults
+	if res[0][0]>res[0][1]:
+	    return "Baseball"
+	else:
+	    return "Cricket"
+	#     deleting the file at last
 
 
 @app.route("/")
 def index():
-    return render_template('home.html')
+    return render_template('index.html')
 
 # request would be made from js code
-@app.route("/predict",methods=["POST","GET"])
-def fetchdata():
-    # what if user didnot apply any filter
-    x=request.form.get("url")
-    res=weAreLive(x)
-    return json.dumps({"res":res})
+@app.route("/predict",methods=["POST"])
+def predict():
+	print("request for prediction has body ",request.json)
+	# what if user didnot apply any filter
+	x=request.json.get("url")
+	print("url for image in .py ",x)
+	# x="https://i.ytimg.com/vi/O5bV5LVVr5A/maxresdefault.jpg"
+	res=weAreLive(x)
+	return jsonify(result=res)
+	# return json.dumps({"res":"scsasa"})
 
 if __name__ == '__main__':
 	app.debug=True
